5e06e7f2b579a0c288318680ec5b9b82b4b3e390
camrynalexx <camryn.cam@gmail.com>
2021-11-15 03:15:12 -0800

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -10,35 +10,62 @@ function formatDate(timestamp) {
       minutes = `0${minutes}`;
   }
 
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+  ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = [
+  "Sun",
+  "Mon",
+  "Tues",
+  "Wed",
+  "Thurs",
+  "Fri",
+  "Sat",
+];
+
+
+return days[day];
+}
+
 function displayForecast(response) {
- console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement= document.querySelector("#forecast");
 
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
-
   let forecastHTML = `<div class = "row">`;
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+  if (index < 6) {
+
   forecastHTML = 
-  forecastHTML + 
+    forecastHTML + 
    ` 
               <div class="col">
-                <div class="weather-forecast-date">${day}</div>
-                <img src="http://openweathermap.org/img/wn/04n@2x.png" alt="" width="36" />
+                <div class="weather-forecast-date">${formatDay (forecastDay.dt)}</div>
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36" />
                <div class="weather-forecast-temperatures">
                  <span class="weather-forecast-temperature-max">
-                   18°
+                   ${Math.round(forecastDay.temp.max)}°
                  </span>
                  <span class="weather-forecast-temperature-min">
-                   12°
+                   ${Math.round(forecastDay.temp.min)}°
                 </span>
                 </div>
               </div>
             `;
+  };
   });
 
   forecastHTML = forecastHTML + `</div>`;         
@@ -123,4 +150,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
-search("New York");
+search("San Francisco");
+
