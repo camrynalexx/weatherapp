01a9a549b01cb42f687a02a07afdfe2dda5422ec
camrynalexx <camryn.cam@gmail.com>
2021-11-15 20:42:09 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,56 +7,50 @@ function formatDate(timestamp) {
 
   let minutes = date.getMinutes();
   if (minutes < 10) {
-      minutes = `0${minutes}`;
+    minutes = `0${minutes}`;
   }
 
   let days = [
-    "Sunday", 
-    "Monday", 
-    "Tuesday", 
-    "Wednesday", 
-    "Thursday", 
-    "Friday", 
-    "Saturday"
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
   ];
 
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
-  }
+}
 
 function formatDay(timestamp) {
-let date = new Date(timestamp * 1000);
-let day = date.getDay();
-let days = [
-  "Sun",
-  "Mon",
-  "Tues",
-  "Wed",
-  "Thurs",
-  "Fri",
-  "Sat",
-];
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
-
-return days[day];
+  return days[day];
 }
 
 function displayForecast(response) {
   let forecast = response.data.daily;
 
-  let forecastElement= document.querySelector("#forecast");
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class = "row">`;
   forecast.forEach(function (forecastDay, index) {
-  if (index < 6) {
-
-  forecastHTML = 
-    forecastHTML + 
-   ` 
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
               <div class="card col-sm-2" id="forecast-card">
-               <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+               <div class="weather-forecast-date">${formatDay(
+                 forecastDay.dt
+               )}</div>
                 <center>
-                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40" class="weatherIcon"/>
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" width="40" class="weatherIcon"/>
                 </center>
                <div class="weather-forecast-temperatures">
                  <span class="weather-forecast-temperature-max">
@@ -68,10 +62,10 @@ function displayForecast(response) {
                 </div>
               </div>
             `;
-  };
+    }
   });
 
-  forecastHTML = forecastHTML + `</div>`;         
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
@@ -90,7 +84,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon")
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -98,26 +92,28 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-   getForecast(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-    let apiKey = "281450ec88936f4fa8ee9864682b49a0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "281450ec88936f4fa8ee9864682b49a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-
 function handleSubmit(event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
 search("New York");
